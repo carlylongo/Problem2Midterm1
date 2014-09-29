e7a6577514540b61065935491f63a10bccc8cc80
johnn_000 <johnn_000@Akuma-XPS.home>
2014-09-29 18:17:49 -0400

    Junit Assignment

---
+++ CISC181.Midterm1/src/MainPackage/checkReturnTrue.java
@@ -0,0 +1,12 @@
+package MainPackage;
+
+
+import junit.framework.TestCase;
+
+public class checkReturnTrue extends TestCase {
+
+	public void testReturnTrue() {
+		assertEquals("Test passed",true, true );
+	}
+
+}
